7f8f6c3dff196b6a195b0e913a34be8295ebd961
eva <me@example.com>
2020-01-30 11:46:09 +0100

    Updates

---
+++ precache-manifest.6c77f670e70a2b78bc4d93324037d997.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06715ac06c8c552839ad95836c4e8264",
+    "url": "/evagrean/meetup-app.git/index.html"
+  },
+  {
+    "revision": "85b414501e62e7d0a5ff",
+    "url": "/evagrean/meetup-app.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3ee8d8a1096c7ef0d46a",
+    "url": "/evagrean/meetup-app.git/static/js/2.efea5617.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/evagrean/meetup-app.git/static/js/2.efea5617.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85b414501e62e7d0a5ff",
+    "url": "/evagrean/meetup-app.git/static/js/main.9d55719b.chunk.js"
+  },
+  {
+    "revision": "68ecc6438609818021b5",
+    "url": "/evagrean/meetup-app.git/static/js/runtime-main.b398918e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/evagrean/meetup-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
