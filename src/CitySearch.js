d84c88fce97442da10add74389e7c336d295e5f6
eva <me@example.com>
2020-02-20 09:15:00 +0100

    added logic for InfoAlert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getSuggestions } from './api';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
@@ -10,7 +11,20 @@ class CitySearch extends Component {
   handleInputChanged = (event) => {
     const value = event.target.value;
     this.setState({ query: value });
-    getSuggestions(value).then(suggestions => this.setState({ suggestions }));
+    getSuggestions(value).then(suggestions => {
+      this.setState({ suggestions });
+
+      if (value && suggestions.length === 0) {
+        this.setState({
+          infoText: 'We can not find the city you are looking for. Please try another city',
+        });
+      } else {
+        this.setState({
+          infoText: '',
+        });
+      }
+    });
+
   }
 
   handleItemClicked = (value, lat, lon) => {
@@ -22,6 +36,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input type="text" className="city" value={this.state.query} placeholder="City" onChange={this.handleInputChanged} />
         <ul className="suggestions">
           {this.state.suggestions.map(item =>
